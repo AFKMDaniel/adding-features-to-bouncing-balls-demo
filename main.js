d510ba6f98dddb76c737a1526afe4ac6c5369db3
AFKMDaniel <danich734@gmail.com>
2022-07-30 22:32:41 +1000

    Defining methods for EvilCircle

--- main.js
+++ main.js
@@ -77,9 +77,9 @@ class Ball extends Shape {
           if (distance < this.size + ball.size) {
             ball.color = this.color = randomRGB();
           }
+        }
       }
-    }
-  }
+    } 
 }
 
 class EvilCircle extends Shape {
@@ -105,6 +105,46 @@ class EvilCircle extends Shape {
           break;
       }
     });    
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+
+  update() {
+    if ((this.x + this.size) >= width) {
+       this.x = this.x + this.size;
+    }
+ 
+    if ((this.x - this.size) <= 0) {
+      this.x = this.x - this.size;
+    }
+ 
+    if ((this.y + this.size) >= height) {
+      this.y = this.y + this.size;
+    }
+ 
+    if ((this.y - this.size) <= 0) {
+      this.y = this.y - this.size;
+    }
+ }
+
+ collisionDetect() {
+  for (const ball of balls) {
+    if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.exists = false;
+        }
+      }
+    }
   }
 
 }
